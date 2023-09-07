518e4c9bffbf70bd44269e32178e0b640b5ebfc3
Muhammad Fiqri <muhfikri6565@gmail.com>
2023-09-07 16:03:09 +0700

    reset button made

--- Script/figurine_puzzle_click.gd
+++ Script/figurine_puzzle_click.gd
@@ -2,6 +2,14 @@ extends Node2D
 
 @onready var starting_snap_pos_group = [$SnapPosition1,$SnapPosition2,$SnapPosition3,$SnapPosition4,$SnapPosition6,$SnapPosition7]
 @onready var figurine_group = [$%FigurineRed,$%FigurineYellow,$%FigurineGreen,$"%FigurineBlue",$"%FigurinePurple",$"%FigurineBlack"]
+@onready var starting_position = {
+	$SnapPosition1 : null,
+	$SnapPosition2 : null,
+	$SnapPosition3 : null,
+	$SnapPosition4 : null,
+	$SnapPosition6 : null,
+	$SnapPosition7 : null,
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,3 +26,19 @@ func randomize_figurine_location():
 		i.owner = starting_snap_pos_group[position_chosen]
 		i.global_position = starting_snap_pos_group[position_chosen].global_position
 		starting_snap_pos_group.pop_at(position_chosen)
+	
+	save_start_position()
+
+func save_start_position():
+	var starting_snap = [$SnapPosition1,$SnapPosition2,$SnapPosition3,$SnapPosition4,$SnapPosition6,$SnapPosition7]
+	for i in starting_snap:
+		var figurine = i.find_child("Figurine*",true,false)
+		starting_position[i] = figurine
+
+func _on_reset_button_button_down():
+	for i in starting_position:
+		var position = i
+		var figurine = starting_position[i]
+		figurine.reparent(position)
+		figurine.owner = position
+		figurine.global_position = position.global_position
